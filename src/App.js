165f99559d426fd77912029b1fd7cc38acef0562
findmory <findmory@gmail.com>
2020-04-23 11:34:29 -0400

    refactor to Provide all stores as "stores" and then inject this rootstore

--- src/App.js
+++ src/App.js
@@ -5,25 +5,33 @@ import { compose } from "recompose";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // deconstruct the stores for convienence
+    const { clubStore, firebaseStore } = props.stores;
+    this.clubStore = clubStore;
+    this.firebaseStore = firebaseStore;
+  }
   componentDidMount() {
     // listen to changes in the 'clubs' ref
     // and set mobx store `clubs` with the firebase clubs object
     console.log(this.props);
-    this.props.firebaseStore.clubs().on("value", (snapshot) => {
-      this.props.clubStore.setClubs(snapshot.val());
+    this.firebaseStore.clubs().on("value", (snapshot) => {
+      this.clubStore.setClubs(snapshot.val());
     });
   }
 
   componentWillUnmount() {
-    this.props.firebaseStore.clubs().off();
+    this.firebaseStore.clubs().off();
   }
 
   render() {
-    if (!this.props.firebaseStore) {
+    if (!this.firebaseStore) {
       return null;
     }
 
-    const clubList = this.props.clubStore.clubList;
+    const clubList = this.clubStore.clubList;
     console.log(clubList);
 
     let list = clubList
@@ -39,4 +47,4 @@ class App extends Component {
 }
 
 // inject makes the stores available on `this.props`
-export default compose(inject("clubStore", "firebaseStore"), observer)(App);
+export default compose(inject("stores"), observer)(App);
--- src/stores/ClubStore.js
+++ src/stores/ClubStore.js
@@ -3,9 +3,12 @@ import { observable, action, computed, decorate } from "mobx";
 class ClubStore {
   clubs = null;
 
-  // constructor(rootStore) {
-  //   this.rootStore = rootStore;
-  // }
+  constructor(rootStore) {
+    // if he needs access to any other store
+    // it's available on this.rootStore.<other_store>
+    this.rootStore = rootStore;
+    console.log(this.rootStore);
+  }
 
   setClubs = (clubs) => {
     this.clubs = clubs;
